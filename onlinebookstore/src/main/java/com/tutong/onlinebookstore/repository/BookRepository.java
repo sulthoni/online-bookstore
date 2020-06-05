783d3964744cabd19e97b12ffb13afb3b6ea861f
SULTHONI <sulthoni@bps.go.id>
2020-06-06 00:06:09 +0700

    Completed the feature 3 search by category

--- onlinebookstore/src/main/java/com/tutong/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/tutong/onlinebookstore/repository/BookRepository.java
@@ -1,11 +1,17 @@
 package com.tutong.onlinebookstore.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.tutong.onlinebookstore.entity.Book;
 
 @CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+	
+	@RestResource(path = "category")
+	Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
